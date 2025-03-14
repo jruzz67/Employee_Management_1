ec8386b6e9e37bae7124db418e3c6309b50e585b
jruzz67 <j1567ruzz@gmail.com>
2025-03-14 10:58:06 +0530

    Update EmployeeRepo.java

--- springapp/src/main/java/com/examly/springapp/repository/EmployeeRepo.java
+++ springapp/src/main/java/com/examly/springapp/repository/EmployeeRepo.java
@@ -1,5 +1,13 @@
 package com.examly.springapp.repository;
 
-public class EmployeeRepo {
-    
+import com.examly.springapp.model.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface EmployeeRepo extends JpaRepository<Employee, Integer> {
+
+    List<Employee> findByName(String name);
+
+    List<Employee> findByDesignation(String designation);
 }
